f1b2a260e839ace1fcd6bb04a33748f07bcd91af
fisk <fiskie@live.co.uk>
2021-08-21 04:21:06 +0100

    add GetUserBans

---
+++ src/Marketplace/Command/Partner/GetUserBans.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fisk
+ * Date: 03/06/17
+ * Time: 00:37
+ */
+
+namespace Marketplace\Command\Partner;
+
+
+use Marketplace\Command\CommandInterface;
+
+class GetUserBans implements CommandInterface {
+    private $steamid;
+
+    public function __construct(string $steamid) {
+        $this->steamid = $steamid;
+    }
+
+    public function getRequestMethod(): string {
+        return 'GET';
+    }
+
+    public function getInterface(): string {
+        return 'Partner';
+    }
+
+    public function getMethod(): string {
+        return 'GetUserBans';
+    }
+
+    public function getVersion(): string {
+        return 'v1';
+    }
+
+    public function getParams(): array {
+        return [
+            'steamid' => $this->steamid
+        ];
+    }
+}
